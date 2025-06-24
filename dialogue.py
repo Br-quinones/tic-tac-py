c5826cbed2530cf2f689318db9838cca2e5f0b8b
Br_Quinones <br-quinones@protonmail.com>
2025-06-23 20:23:23 -0500

    v0.09

--- dialogue.py
+++ dialogue.py
@@ -1,6 +1,7 @@
 import sys; import json; import time
-import msvcrt
+import msvcrt; import art
 from colorama import Fore
+import os
 
 #Abrir .json para idiomas 
 with open("languages/spanish.json", "r", encoding="utf-8") as file:
@@ -10,30 +11,32 @@ def deletreo(word):
     for letter in word:
         sys.stdout.write(letter)
         sys.stdout.flush()
-        time.sleep(0.02)
-    print("\n")
+        time.sleep(0.015)
     msvcrt.getch()
+    os.system("cls")
 
 class message:
     def agatha(text):
+        art.agatha_art()
         print(Fore.GREEN + "Agatha: " + Fore.RESET ,end="")
         deletreo(text)
 
-    def beatriz(text):
+    def beatrice(text):
+        art.beatrice_art()
         print(Fore.MAGENTA + "Beatriz: " + Fore.RESET ,end="")
         deletreo(text)
 
     def victoria(text):
+        art.victoria_art()
         print(Fore.YELLOW + "Victoria: " + Fore.RESET ,end="")
         deletreo(text)
 
-
 def chapter_01():
-    message.agatha("En un lugar de la mancha de cuyo nombre no quisiera recordar no ha mucho tiempo.")
+    message.agatha("En un lugar de la Mancha, de cuyo nombre no quiero acordarme.")
     message.victoria("¿Pero que estas hablando mujer?")
     message.agatha("Oh tan solo estaba leyendo un libro que encontre.")
-    message.beatriz("Hey dejan de perder el tiempo con libros y entrenamos.")
-    message.agatha("Si mejor, dejare esto por un momento.")
+    message.beatrice("Hey si dejan de perder el tiempo con libros y entrenamos.")
+    message.agatha("Bien beatrice, entrenemos entonces")
     message.victoria("Bueno Agatha, te explico como funciona el juego, usando tu teclado físico...")
-    message.beatriz("Panplinas el juego es tan facil, vamos de frente a la acción.")
-    message.beatriz("Ademas la mejor forma de aprender es mediante la paliza.")
+    message.beatrice("Ahhh, panplinas el juego es tan facil, vamos de frente a la acción.")
+    message.beatrice("Ademas la mejor forma de aprender es mediante la paliza.")
