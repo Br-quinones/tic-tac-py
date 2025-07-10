b185fc47763181c436190605a32d6a505dd8fb34
Br_Quinones <dadidhuisa@gmail.com>
2025-07-09 21:55:24 -0500

    v0.26

---
+++ languages/ja.py
@@ -0,0 +1,33 @@
+############ Main menu ###########
+#Menu 
+play = "プレイ"
+languages = "言語"
+credits = "クレジット"
+exit = "終了"
+
+#idiomas
+choice_language = "言語を選択してください: "
+spanish = "スペイン語"
+english = "英語"
+japanese = "日本語"
+
+#Creditos
+author = "作成者: Brayan Quiñones"
+traductor = "翻訳者: Gemeni & Chat GPT"
+
+#Salir
+exiting = "終了しています..."
+
+############## Tic-Tac-Toe ##############
+key = "キー"
+
+#Procesos
+ending_game = "ゲームを終了しています..."
+processing_game = "ゲームを処理しています..."
+
+#Resultado para el ganador
+win_the = "勝者: "
+player = "プレイヤー"
+enemy = "敵"
+draw = "引き分け"
+input_for_exit = "終了するには任意のキーを押してください"
