8cdcdd8f3a31d7222478b42a775b4f45df67fe36
Br_Quinones <br-quinones@protonmail.com>
2025-07-16 21:26:21 -0500

    v0.33

--- dialogue.py
+++ dialogue.py
@@ -1,13 +1,17 @@
-import sys; import json; import time
+import sys; import time; import os
 import msvcrt; import art
 from colorama import Fore
-import os
+from pygame import mixer 
 
-def deletreo(word):
+mixer.init()
+def deletreo(word, bleep):
+    mixer.music.load(f"audio/{bleep}.ogg")
+    mixer.music.play(-1)
     for letter in word:
         sys.stdout.write(letter)
         sys.stdout.flush()
         time.sleep(0.015)
+    mixer.music.stop() 
     msvcrt.getch()
     os.system("cls")
 
@@ -15,34 +19,25 @@ class message:
     def agatha(text):
         art.agatha_ascci()
         print(Fore.GREEN + "Agatha: " + Fore.RESET ,end="")
-        deletreo(text)
+        deletreo(text, "bleep001")
 
     def beatrice(text):
         art.beatrice_ascci()
         print(Fore.MAGENTA + "Beatriz: " + Fore.RESET ,end="")
-        deletreo(text)
+        deletreo(text, "bleep002")
 
     def victoria(text):
         art.victoria_ascci()
         print(Fore.YELLOW + "Victoria: " + Fore.RESET ,end="")
-        deletreo(text)
+        deletreo(text, "bleep005")
 
     def director(text):
         art.directo_ascci()
         print(Fore.RED + "Director: " + Fore.RESET ,end="")
-        deletreo(text)
+        deletreo(text, "bleep004")
 
     def role(text):
         art.role_ascci()
         print(Fore.CYAN + "Roleo: " + Fore.RESET , end="")
-        deletreo(text)
+        deletreo(text, "bleep003")
 
-import tictactoe
-def chapter_01():
-    message.role("Observas que dentro de una salón una muchedumbre rodea algo.")
-    message.role("Ese algo no es más que una intensa partida del 3 en raya entre dos chicas")
-    message.victoria("Otra persona aplastada, por mi gran equipo!")
-    message.victoria("Oye tu, si tu él que acaba de llegar ¿una ronda que dices?")
-    message.agatha("Victoria... dejame practicar, esta persona tambien es principiante a igual que yo")
-    message.victoria("pufff, bueno practica entonces")
-    tictactoe.the_game("easy")
