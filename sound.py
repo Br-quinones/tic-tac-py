d9f6028b1cd645cf04e922e3da731fb084433e2f
Br_Quinones <br-quinones@protonmail.com>
2025-07-20 21:21:47 -0500

    v0.37

--- sound.py
+++ sound.py
@@ -6,9 +6,9 @@ def star_sound_loop(bleep):
     mixer.music.load(f"audio/loops/{bleep}.ogg")
     mixer.music.play(-1)
 
-def star_sound_efect(efect):
-    mixer.music.load(f"audio/{efect}")
+def star_sound_efect(file_the_of_audio):
+    mixer.music.load(file_the_of_audio)
     mixer.music.play()
 
-def star_sound_efect_channel():
-    mixer.Channel(1).play(mixer.Sound("audio/move.mp3"))
+def star_sound_efect_channel(file_the_of_audio):
+    mixer.Channel(1).play(mixer.Sound(file_the_of_audio))
