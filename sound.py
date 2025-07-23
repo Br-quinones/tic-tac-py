3f2cfd8b6e0a4cb096fdbc705db6241a4e67a6f2
Br_Quinones <dadidhuisa@gmail.com>
2025-07-22 21:40:32 -0500

    v0.39

--- sound.py
+++ sound.py
@@ -1,13 +1,11 @@
-from pygame import mixer 
+from pygame import mixer #Modulos publicos
 
 #Channel_0: Musica de fondo // .mp3
 #Channel_1: Efectos de sonido // .wav
 #Channel_2: Bucles de sonido // .ogg
 
-def star_sounds():
-    mixer.init()
-
 def music_sound(music):
+    mixer.init()
     mixer.Channel(0).play(mixer.Sound(f"audio/musics/{music}"), 0)
 
 def effect_sound(effect):
